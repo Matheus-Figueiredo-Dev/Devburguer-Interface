bf44c409c1059daa8af1924a817c17ca0e23c85c
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2025-03-14 16:30:49 -0300

    Iniciando a tela de login

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,7 +1,41 @@
-export function Login(){
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
+import {
+  Button,
+  Container,
+  Form,
+  InputContainer,
+  LeftContainer,
+  Link,
+  RightContainer,
+  Title,
+} from './styles';
+import Logo from '../../assets/logo.svg';
+
+export function Login() {
+  return (
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt="Logo-DevBurger" />
+      </LeftContainer>
+      <RightContainer>
+        <Title>
+          Olá, seja bem vindo ao <span>Dev Burguer</span>! Acesse com seu{' '}
+          <span>Login e senha</span>
+        </Title>
+        <Form>
+          <InputContainer>
+            <label>Email</label>
+            <input type="email" />
+          </InputContainer>
+
+          <InputContainer>
+            <label>Senha</label>
+            <input type="password" />
+          </InputContainer>
+          <Link>Esqueci minha senha</Link>
+          <Button>Entrar</Button>
+        </Form>
+        <Link>Não possui conta? Clique aqui</Link>
+      </RightContainer>
+    </Container>
+  );
 }
