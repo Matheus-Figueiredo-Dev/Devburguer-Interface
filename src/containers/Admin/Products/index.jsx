14eb8550d43bb0df4d93e292c01fc0fc3c0ebbdb
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2025-03-29 16:07:37 -0300

    FInalizando tela de produtos

--- src/containers/Admin/Products/index.jsx
+++ src/containers/Admin/Products/index.jsx
@@ -1,7 +1,81 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { CheckCircle, Pencil, XCircle } from '@phosphor-icons/react';
+
+import { api } from '../../../services/api';
+import { Container, EditButton, ProductImage } from './syles';
+import { formatPrice } from '../../../utils/formatPrice';
+
 export function Products() {
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function loadProducts() {
+      const { data } = await api.get('/products');
+
+      console.log(data);
+      setProducts(data);
+    }
+    loadProducts();
+  }, []);
+
+  function isOffer(offer) {
+    if (offer) {
+      return <CheckCircle color="#61a120" size={25} />;
+    } else {
+      return <XCircle color="#8B0000" size={25} />;
+    }
+  }
+
+  function editProduct(product) {
+    navigate('/admin/editar-produto', { state: { product } });
+  }
+
   return (
-    <div>
-      <h2>Produtos</h2>
-    </div>
+    <Container>
+      <TableContainer component={Paper} sx={{ overflowX: 'auto' }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Nome</TableCell>
+              <TableCell align="center">Preço</TableCell>
+              <TableCell align="center">Produto em oferta</TableCell>
+              <TableCell align="center"></TableCell>
+              <TableCell align="center">Editar</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map((product) => (
+              <TableRow
+                key={product.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell>{product.name}</TableCell>
+                <TableCell align="center">
+                  {formatPrice(product.price)}
+                </TableCell>
+                <TableCell align="center">{isOffer(product.offer)}</TableCell>
+                <TableCell align="center">
+                  <ProductImage src={product.url} />
+                </TableCell>
+                <TableCell align="center">
+                  <EditButton onClick={() => editProduct(product)}>
+                    <Pencil />
+                  </EditButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 }
